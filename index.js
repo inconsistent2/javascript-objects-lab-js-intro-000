1973fa210adeff41a31f3ee2366d3ea9fa9fdd4e
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 11:15:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 
  function deleteFromObjectByKey(obj,key){
    var newobj=Object.assign({},obj)
-   delete newobj.key
+   delete newobj[key]
    return newobj
     }
