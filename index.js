90a5da90a0105d2f939cacfeb41d43b1d6d81a26
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 10:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var recipes={};
 function updateObjectWithKeyAndValue(object, key, value){
-var newrecipe=object.assign({},recipe)
+var newrecipe=object.assign({},recipes)
 newrecipe[key]=value
   return newrecipe
 }
